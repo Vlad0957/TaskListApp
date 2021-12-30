7b0ea31fe7e93c1daa14dace2b2a36ad0e267da2
Vlad0957 <daron-2010-777@yandex.ru>
2021-12-30 21:59:22 +0300

    21:59/30.12

--- my-app/src/redux/saga/sagas.js
+++ my-app/src/redux/saga/sagas.js
@@ -1,18 +1,32 @@
 import {takeEvery, put, call} from 'redux-saga/effects'
-
-import {REQUEST_TASKS} from '../actions/type';
+import {REQUEST_TASKS, ADD_TASK} from '../actions/type';
 import { addFetchTasks } from '../actions/actions';
+
+
 
 export default function* sagaWatcher(){
   yield takeEvery(REQUEST_TASKS, sagaWorker)
+  yield takeEvery(ADD_TASK, sagaWorkerAddTask)
 }
 
 function* sagaWorker(){
+
 const payload =  yield call(fetchTasks)
 yield put(addFetchTasks({
   payload
 }))
 }
+
+function* sagaWorkerAddTask(data){
+  console.log(data.payload.text)
+
+  const payload = yield call(createNewTask, data)
+  console.log(payload)
+  yield put(addFetchTasks({
+    payload
+  }))
+ 
+  }
 
 async function fetchTasks(){
   console.log('Fetch request')
@@ -21,3 +35,24 @@ async function fetchTasks(){
   console.log(res)
   return res
 }
+
+async function createNewTask(payload){
+  console.log('addFetchTasks ', payload.payload)
+  const response = await fetch('https://uxcandy.com/~shapoval/test-task-backend/v2/?developer=Vlad/create', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: payload.payload.username,
+      email: payload.payload.email,
+      text: payload.payload.text
+    }),
+  });
+  // console.log(response.json())
+  let res = response.json()
+  console.log(res)
+  return res
+}
+
+
