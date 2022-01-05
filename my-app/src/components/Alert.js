6eeeb39e12b217552ba51b7aa640d94d1a0488de
Vlad0957 <daron-2010-777@yandex.ru>
2022-01-05 12:49:19 +0300

    addSwitchOnNextPage

--- my-app/src/components/Alert.js
+++ my-app/src/components/Alert.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { hideAlert, clearState, sortByParam } from '../redux/actions/actions';
 import {styleAlert} from '../styles.js'
@@ -11,21 +11,28 @@ export default function Alert() {
   const state = useSelector((state) => state);
   const curentSort = useSelector((state) => state.currentPage);
   const dispatch = useDispatch();
+  const [someTest, setSomeTest] = useState(false)
+  const [thisPage, setThisPage] = useState(state.currentPage.pageNum)
+  if(answer == 'Task created' && state.currentPage.pageNum == Math.ceil(state.page[0].total_count/3) && state.page[0].tasks.length == 3){
+    setSomeTest(true)
+    setThisPage(++state.currentPage.pageNum)
+  }
   function handleClick(e) {
     e.preventDefault();
     
-    dispatch(hideAlert());
     dispatch(clearState({
-
+      
     }));
-
+    
     dispatch(sortByParam({
       value: {
         field: curentSort.field,
         direction: curentSort.direct,
       },
-      num: state.currentPage.pageNum,
-    }));
+      num: thisPage,
+    }))
+      
+    dispatch(hideAlert());
   }
   return (
 
