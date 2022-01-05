f577911184195ee0a21b6ce45eb04cea884c5676
Vlad0957 <daron-2010-777@yandex.ru>
2022-01-05 16:36:59 +0300

    Eslint

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,55 +1,48 @@
-import {React, useState} from 'react'
-import Page from './components/Page'
-import Form from './components/Form'
+import { React, useState } from 'react';
+import Page from './components/Page';
+import Form from './components/Form';
 import SortBar from './components/SortBar';
 import HeaderBar from './components/HeaderBar';
-import {styleApp} from './styles'
-
-
+import { styleApp } from './styles';
 
 function App() {
-  const styles = styleApp
-  const [viewForm, setViewForm] = useState(false)
-  const [viewButton, setViewButton] = useState(true)
-  function handleClick(e){
-    e.preventDefault()
-    setViewForm(true)
-    setViewButton(false)
+  const styles = styleApp;
+  const [viewForm, setViewForm] = useState(false);
+  const [viewButton, setViewButton] = useState(true);
+  function handleClick(e) {
+    e.preventDefault();
+    setViewForm(true);
+    setViewButton(false);
   }
-  function changeViewForm(){
-    setViewForm(false)
-    setViewButton(true)
+  function changeViewForm() {
+    setViewForm(false);
+    setViewButton(true);
   }
   return (
-  
+
     <div style={styles.div} className="Container">
       <HeaderBar />
-    <div
-    style={{
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}
-    >
-      <SortBar />
-    {viewForm != true && viewButton == true
-        &&
-      <button style={styles.button} onClick={handleClick} type="button" class="btn btn-primary btn-sm">Add task</button>  
-      }
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <SortBar />
+        {viewForm != true && viewButton == true
+        && <button style={styles.button} onClick={handleClick} type="button" className="btn btn-primary btn-sm">Add task</button>}
 
+      </div>
+      <div style={styles.divTwo}>
+        <Page />
+
+        { viewForm == true
+        && <Form changeViewForm={changeViewForm} />}
+      </div>
     </div>
-    <div style={styles.divTwo}>
-      <Page />
-     
-      { viewForm == true
-        &&
-      <Form changeViewForm={changeViewForm}/>
-      }
-    </div>
-    </div>
-    
-  
+
   );
 }
 
--- my-app/src/components/Alert.js
+++ my-app/src/components/Alert.js
@@ -1,37 +1,35 @@
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { hideAlert, clearState, sortByParam } from '../redux/actions/actions';
-import {styleAlert} from '../styles.js'
-
-
+import { styleAlert } from '../styles.js';
 
 export default function Alert() {
-  const styles = styleAlert
+  const styles = styleAlert;
   const answer = useSelector((state) => state.answer.message);
   const state = useSelector((state) => state);
   const curentSort = useSelector((state) => state.currentPage);
   const dispatch = useDispatch();
-  const [someTest, setSomeTest] = useState(false)
-  const [thisPage, setThisPage] = useState(state.currentPage.pageNum)
-  if(answer == 'Task created' && state.currentPage.pageNum == Math.ceil(state.page[0].total_count/3) && state.page[0].tasks.length == 3){
-    setSomeTest(true)
-    setThisPage(++state.currentPage.pageNum)
+  // const [someTest, setSomeTest] = useState(false)
+  const [thisPage, setThisPage] = useState(state.currentPage.pageNum);
+  if (answer == 'Task created' && state.currentPage.pageNum == Math.ceil(state.page[0].total_count / 3) && state.page[0].tasks.length == 3) {
+    // setSomeTest(true)
+    setThisPage(++state.currentPage.pageNum);
   }
   function handleClick(e) {
     e.preventDefault();
-    
+
     dispatch(clearState({
-      
+
     }));
-    
+
     dispatch(sortByParam({
       value: {
         field: curentSort.field,
         direction: curentSort.direct,
       },
       num: thisPage,
-    }))
-      
+    }));
+
     dispatch(hideAlert());
   }
   return (
